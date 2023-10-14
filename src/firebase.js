c2ee3cb300f014abd6788932abfb478852c19659
won <januse159@naver.com>
2023-10-14 20:40:02 +0900

    Creating a shopping mall front page

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8m7ibeiYmjZr6UjhE39PUDuigNtj6Lbw",
+  authDomain: "react-shop-app-59754.firebaseapp.com",
+  projectId: "react-shop-app-59754",
+  storageBucket: "react-shop-app-59754.appspot.com",
+  messagingSenderId: "933427720722",
+  appId: "1:933427720722:web:ac1569aea699d2a5b7decb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
