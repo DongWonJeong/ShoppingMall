c5aa6b2ff46f29a13614f8bb6df49f7a0b40a29d
DongWonJeong <januse159@naver.com>
2023-10-15 11:51:07 +0900

    Complete the shopping cart function

--- src/pages/HomePage/card-list/card-item/CardItem.jsx
+++ src/pages/HomePage/card-list/card-item/CardItem.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 import styles from './CardItem.module.scss';
-import { useAppSelector } from '../../../../hooks/redux';
+import { useAppDispatch, useAppSelector } from '../../../../hooks/redux';
+import { addToCart } from '../../../../store/cart/cart.slice';
 
 
 const CardItem = ({item}) => {
 
   const { products } = useAppSelector(state => state.cartSlice);
   const productMatching = products.some((product) => product.id === item.id);
+  const dispatch = useAppDispatch();
+  const addItemTOCart = () => {
+    dispatch(addToCart(item));
+  }
+
   return (
     <li className ={styles.card_item}>
         <Link to={`/product/${item.id}`}>
@@ -24,6 +30,7 @@ const CardItem = ({item}) => {
         <div>
             <button
                 disabled={productMatching}
+                onClick={() => !productMatching && addItemTOCart()}
             >
                 {productMatching ? "장바구니에 담긴 제품" : "장바구니에 담기"}
             </button>
--- src/store/cart/cart.slice.js
+++ src/store/cart/cart.slice.js
@@ -1,16 +1,113 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//Mock API 연동
+export const postOrder = createAsyncThunk(
+    "cart/postOrder",
+    async (order, thunkAPI) => {
+        try {
+            await axios.post("https://652b4f484791d884f1fdb504.mockapi.io/orders",
+                order
+            )
+            thunkAPI.dispatch(sendOrder())
+        } catch (error) {
+            return thunkAPI.rejectWithValue("Error sending order");
+        }
+    }
+)
+
+
 
 const initialState = {
-    products: [],
+    products: localStorage.getItem("cartProducts") ?
+        JSON.parse(localStorage.getItem("cartProducts")) : [],
     totalPrice: 0,
-    userId: ""
+    userId: localStorage.getItem("userId") ?
+        JSON.parse(localStorage.getItem("userId")) : "",
 }
 
 
 export const cartSlice = createSlice({
     name: "cart",
     initialState,
-    reducers: {}
+    reducers: {
+        setUserId: (state, action) => {
+            state.userId = action.payload;
+
+            localStorage.setItem('userId', JSON.stringify(state.userId));
+        },
+        
+        removeUserId: (state, action) => {
+            state.userId = "";
+
+            localStorage.setItem('userId', JSON.stringify(state.userId));
+        },
+
+        addToCart: (state, action) => {
+            state.products.push({
+                ...action.payload,
+                quantity: 1,
+                total: action.payload.price
+            })
+
+            localStorage.setItem('cartProducts', JSON.stringify(state.products));
+        },
+
+        deleteFromCart: (state, action) => {
+            state.products = state.products.filter((item) => item.id !== action.payload)
+
+            localStorage.setItem('cartProducts', JSON.stringify(state.products));
+        },
+
+        incrementProduct: (state,action) => {
+            state.products = state.products.map((item) => 
+                item.id === action.payload
+                    ? {
+                        ...item,
+                        quantity: item.quantity +1,
+                        total: item.price * (item.quantity+1)
+                    }
+                    : item
+            )
+            localStorage.setItem('cartProducts', JSON.stringify(state.products));
+        },
+
+        decrementProduct: (state,action) => {
+            state.products = state.products.map((item) => 
+                item.id === action.payload
+                    ? {
+                        ...item,
+                        quantity: item.quantity -1,
+                        total: item.price * (item.quantity-1)
+                    }
+                    : item
+            )
+            localStorage.setItem('cartProducts', JSON.stringify(state.products));
+        },
+        getTotalPrice: (state) => {
+            state.totalPrice = state.products.reduce(
+                (acc, item) => (acc += item.total),
+                0
+            )
+
+            return state;
+        },
+        sendOrder: (state) => {
+            state.products = [];
+            localStorage.setItem('cartProducts', JSON.stringify(state.products));
+        }
+    }
 })
+
+export const{
+    addToCart,
+    sendOrder,
+    deleteFromCart,
+    incrementProduct,
+    decrementProduct,
+    getTotalPrice,
+    setUserId,
+    removeUserId
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
