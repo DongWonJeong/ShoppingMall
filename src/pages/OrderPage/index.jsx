f2ac03f3d4209784f6849c0562082f0cb4f90cdb
DongWonJeong <januse159@naver.com>
2023-10-16 20:35:01 +0900

    Shopping Mall page complete

--- src/pages/OrderPage/index.jsx
+++ src/pages/OrderPage/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth'
+import OrdersList from './orders-list/OrdersList';
 
 const OrderPage = () => {
+
+  const {isAuth} = useAuth();
+
+  //로그인이 안될시 Navigate to="/"경로로 이동
+  if(!isAuth) return <Navigate to="/" />
+
   return (
-    <div>OrderPage</div>
+    <div className='page'>
+      <div className='container'>
+        <h1>주문 목록</h1>
+          <OrdersList />
+      </div>
+
+    </div>
   )
 }
 
